52a5cc35876080300da58607c11b3d76e356df70
Antu Saha <antusaha970@gmail.com>
2024-07-08 20:50:54 +0600

    pagination and ad card added

---
+++ src/components/AdCard/AdCard.jsx
@@ -0,0 +1,79 @@
+/* eslint-disable react/prop-types */
+import "./adcard.css";
+const AdCard = ({ ad }) => {
+  const {
+    advertisement_image,
+    title,
+    created_at,
+    bedrooms,
+    division,
+    rating,
+    category,
+    owner: { first_name, last_name },
+    price,
+  } = ad;
+
+  const cover_image =
+    advertisement_image.length > 0
+      ? advertisement_image[0].image
+      : "http://127.0.0.1:8000/media/rent/advertisement/property_gvw_8vqbb.webp";
+  const dateObject = new Date(created_at);
+  const formattedDate = dateObject.toLocaleString();
+  return (
+    <div className="col-12 col-sm-6 col-md-4">
+      <div className="card card-margin shadow-sm">
+        <div className="card-header no-border">
+          <img src={cover_image} className="card-img-top" alt="cover image" />
+        </div>
+        <div className="card-body pt-0">
+          <div className="widget-49">
+            <div className="widget-49-title-wrapper">
+              <div className="widget-49-meeting-info">
+                <span className="widget-49-pro-title fw-bold">{title}</span>
+                <span className="widget-49-meeting-time">
+                  Uploaded on : {formattedDate}
+                </span>
+              </div>
+            </div>
+            <ul className="widget-49-meeting-points">
+              <li className="widget-49-meeting-item">
+                <span>
+                  Location: {division}{" "}
+                  <i className="fa-solid fa-location-dot"></i>
+                </span>
+              </li>
+              <li className="widget-49-meeting-item">
+                <span>
+                  Bedrooms: {bedrooms} <i className="fa-solid fa-bed"></i>{" "}
+                </span>
+              </li>
+              <li className="widget-49-meeting-item">
+                <span>
+                  Rating: {rating} <i className="fa-solid fa-star"></i>
+                </span>
+              </li>
+              <li className="widget-49-meeting-item">
+                <span>
+                  Owner: {first_name} {last_name}
+                </span>
+              </li>
+            </ul>
+            <p className="categoryBg">
+              Category: <span className="fw-bold">{category}</span>
+            </p>
+            <p className="mt-2">
+              Fare: <span className="fw-bold">{price} BDT</span>{" "}
+            </p>
+            <div className="widget-49-meeting-action mt-3">
+              <a href="#" className="btn btn-sm btn-flash-border-success">
+                View details
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdCard;
--- src/components/Advertisements/Advertisements.jsx
+++ src/components/Advertisements/Advertisements.jsx
@@ -2,15 +2,17 @@ import { useEffect, useState } from "react";
 import "./advertisements.css";
 import client from "../../api_client/api_client";
 import Loader from "../shared/Loader/Loader";
+import AdCard from "../AdCard/AdCard";
 const Advertisements = () => {
   const [advertisements, setAdvertisements] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
   useEffect(() => {
     const fetchAllAdvertisements = async () => {
       try {
         setIsLoading(true);
-        const response = await client.get("/api/advertise/");
-        setAdvertisements(response.data);
+        const response = await client.get(`/api/advertise/?page=${page}`);
+        setAdvertisements(response?.data);
       } catch (error) {
         console.error({ error });
       } finally {
@@ -18,9 +20,21 @@ const Advertisements = () => {
       }
     };
     fetchAllAdvertisements();
-  }, []);
-  console.log(advertisements?.results);
-  console.log(isLoading);
+  }, [page]);
+
+  const handlePagination = (type) => {
+    if (type == "prev") {
+      if (advertisements.previous) {
+        setPage((curState) => curState - 1);
+      }
+    } else {
+      if (advertisements.next) {
+        setPage((curState) => curState + 1);
+      }
+    }
+    // console.log(advertisements);
+  };
+
   return (
     <section className="container-fluid ad-box">
       <div className="row  g-3">
@@ -44,7 +58,41 @@ const Advertisements = () => {
               </select>
             </div>
           </div>
+          <div>
+            <small>Total Result: {advertisements?.count | 0}</small>
+          </div>
           {isLoading && <Loader />}
+          <div className="row my-4 g-3">
+            {advertisements?.results?.map((ad) => (
+              <AdCard key={ad.id} ad={ad} />
+            ))}
+          </div>
+          <div className="d-flex justify-content-between">
+            <div>
+              <small>5 result per page</small>
+            </div>
+            <div>
+              <ul className="pagination">
+                <li className="page-item">
+                  <a
+                    className="page-link"
+                    onClick={() => handlePagination("prev")}
+                  >
+                    Previous
+                  </a>
+                </li>
+
+                <li className="page-item">
+                  <a
+                    className="page-link"
+                    onClick={() => handlePagination("next")}
+                  >
+                    Next
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </section>
