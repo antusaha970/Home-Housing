549a5fce958b262134db19eaa02dd915822c8155
Antu Saha <antusaha970@gmail.com>
2024-07-09 02:04:03 +0600

    added review host form

--- src/components/AdvertisementDetail/AdvertisementDetail.jsx
+++ src/components/AdvertisementDetail/AdvertisementDetail.jsx
@@ -124,6 +124,73 @@ const AdvertisementDetail = () => {
 
       {/* description area */}
 
+      {/* post review form */}
+      <div className="row justify-content-center">
+        <div className="col-12 col-sm-6 col-md-6 my-5">
+          <h5 className="fw-bold base-color fs-3 mt-3 text-center">
+            Post a review{" "}
+          </h5>
+          <form action="">
+            <div className="mb-3">
+              <label htmlFor="emailForContact" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control inputFiledBg"
+                id="emailForContact"
+                placeholder="name@example.com"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="subjectforcontact" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control inputFiledBg"
+                id="subjectforcontact"
+                placeholder="write subject here"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="subjectforcontact" className="form-label">
+                Rating
+              </label>
+              <select
+                className="form-select inputFiledBg"
+                aria-label="Default select example"
+              >
+                <option value="1">1 Star</option>
+                <option value="2">2 Star</option>
+                <option value="3">3 Star</option>
+                <option value="4">4 Star</option>
+                <option value="5" selected>
+                  5 Star
+                </option>
+              </select>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="subject for contact" className="form-label">
+                Review
+              </label>
+              <textarea
+                rows={5}
+                className="form-control inputFiledBg"
+                id="subject for contact"
+                placeholder="write subject here"
+                required
+              />
+            </div>
+            <button className="btn btn-dark">Post</button>
+          </form>
+        </div>
+      </div>
+
+      {/* post review form */}
+
       {/* review area */}
       <div>
         <h5 className="fw-bold base-color fs-3 mt-3 text-center">
@@ -168,7 +235,6 @@ const AdvertisementDetail = () => {
           </div>
         ))}
       </div>
-
       {/* review area */}
     </section>
   );
