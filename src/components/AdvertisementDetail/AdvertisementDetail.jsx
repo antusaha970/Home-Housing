4120edd9fa75e3209734f0d3fecdceebf8de7497
Antu Saha <antusaha970@gmail.com>
2024-07-09 22:04:25 +0600

    send booking request

--- src/components/AdvertisementDetail/AdvertisementDetail.jsx
+++ src/components/AdvertisementDetail/AdvertisementDetail.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable react/prop-types */
 import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import PhotoAlbum from "react-photo-album";
 import client from "../../api_client/api_client";
 import Loader from "../shared/Loader/Loader";
+import Modal from "react-modal";
 import "./AdvertisementDetail.css";
 
 import { IsLoggedInContext } from "../../context/Allcontext";
@@ -22,6 +24,7 @@ const AdvertisementDetail = () => {
   const [reviews, setReviews] = useState([]);
   const [loggedIn, setLoggedIn] = useContext(IsLoggedInContext);
   const [newReviewPosted, setNewReviewPosted] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   let photos = [];
   useEffect(() => {
@@ -105,8 +108,31 @@ const AdvertisementDetail = () => {
     }
   };
 
+  const handleBooking = async () => {
+    if (!loggedIn) {
+      toast.error("Please login first", {
+        position: "top-right",
+      });
+      navigate("/login");
+    } else {
+      const isBooked = details.is_booked;
+      if (isBooked) {
+        toast.warning("This property is booked by another user!", {
+          position: "top-right",
+        });
+      } else {
+        setIsOpen(true);
+      }
+    }
+  };
+
   return (
     <section className="container my-5">
+      <ModelForBooking
+        modalIsOpen={modalIsOpen}
+        id={id}
+        setIsOpen={setIsOpen}
+      />
       {loading && <Loader />}
       {/* Image gallery area */}
       <PhotoAlbum layout="rows" photos={photos} />
@@ -165,7 +191,7 @@ const AdvertisementDetail = () => {
           <h5 className="fw-bold base-color fs-3 my-5">
             Have you liked this property?
           </h5>
-          <button className="btn btn-success ms-2">
+          <button className="btn btn-success ms-2" onClick={handleBooking}>
             Book now <i className="fa-solid fa-square-check"></i>
           </button>
           <button className="btn btn-dark ms-2" onClick={handleAddToFavorite}>
@@ -298,3 +324,68 @@ const AdvertisementDetail = () => {
 };
 
 export default AdvertisementDetail;
+
+const ModelForBooking = ({ modalIsOpen, setIsOpen, id }) => {
+  const { register, handleSubmit } = useForm();
+  Modal.setAppElement("#root");
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+  function closeModal() {
+    setIsOpen(false);
+  }
+  const onSubmit = async (data) => {
+    data = { ...data, property_ad: parseInt(id) };
+    setIsOpen(false);
+    try {
+      const response = await client.post(`/api/bookings/`, data);
+      console.log(response.data);
+      if (response.status == 200) {
+        toast.success("Booking request sent", {
+          position: "top-right",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("This property is already booked", {
+        position: "top-right",
+      });
+    }
+  };
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      style={customStyles}
+      contentLabel="Example Modal"
+    >
+      <div className="p-5 modal-bg">
+        <div className="text-end">
+          <button onClick={closeModal} className="btn btn-warning">
+            close
+          </button>
+        </div>
+
+        <h5 className="text-center my-3">Any message for owner?</h5>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <textarea
+            {...register("message")}
+            className="form-control"
+            rows={5}
+            required
+          ></textarea>
+          <button className="btn btn-dark mt-5" type="submit">
+            Send request
+          </button>
+        </form>
+      </div>
+    </Modal>
+  );
+};
