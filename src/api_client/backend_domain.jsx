cb8ad6ca38345f013c08f8dbc81df692b2d0f6f7
Antu Saha <antusaha970@gmail.com>
2024-07-14 12:59:01 +0600

    changed api client for hosting

--- src/api_client/backend_domain.jsx
+++ src/api_client/backend_domain.jsx
@@ -1,3 +1,3 @@
-// const backendURL = "https://home-housing-backend.onrender.com";
-const backendURL = "http://127.0.0.1:8000";
+const backendURL = "https://home-housing-backend.onrender.com";
+// const backendURL = "http://127.0.0.1:8000";
 export default backendURL;
