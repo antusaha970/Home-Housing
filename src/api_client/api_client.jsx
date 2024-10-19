d272363a479e98473283aad2923e841eb75be6d0
Antu Saha <antusaha970@gmail.com>
2024-10-20 01:10:56 +0600

    added vercel link

--- src/api_client/api_client.jsx
+++ src/api_client/api_client.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const client = axios.create({
-  baseURL: "https://home-housing-backend.onrender.com",
+  baseURL: "https://home-housing-backend.vercel.app",
   // baseURL: "http://127.0.0.1:8000",
 });
 
