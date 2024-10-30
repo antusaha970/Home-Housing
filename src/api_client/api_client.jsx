d83fcad94123b9a0029632896af4931cf36497a4
Antu Saha <antusaha970@gmail.com>
2024-10-30 12:25:08 +0600

    added backend url in api client

--- src/api_client/api_client.jsx
+++ src/api_client/api_client.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 const client = axios.create({
-  // baseURL: "https://home-housing-backend.vercel.app",
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://home-housing-backend.vercel.app",
+  // baseURL: "http://127.0.0.1:8000",
 });
 
 client.interceptors.request.use(
